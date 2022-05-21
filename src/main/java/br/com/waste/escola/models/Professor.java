d2599abdd021bc4dda7a7bbac0d9264a1f14f5fb
Paulino <paulinomendes03@gmail.com>
2022-05-21 20:49:50 -0300

    Cria a entidade professor

---
+++ src/main/java/br/com/waste/escola/models/Professor.java
@@ -0,0 +1,48 @@
+package br.com.waste.escola.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "professor")
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 60)
+    private String nome;
+    @Column(nullable = false, length = 30, unique = true)
+    private String prontuario;
+
+    @Deprecated // Só o Hibernate usa
+    public Professor() {}
+
+    public Professor(String nome, String prontuario) {
+        this.nome = nome;
+        this.prontuario = prontuario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getProntuario() {
+        return prontuario;
+    }
+
+    public void setProntuario(String prontuario) {
+        this.prontuario = prontuario;
+    }
+}
+
