8ab8a61df50375fed4e969f2ddbb260b8b4a7656
Paulino <paulinomendes03@gmail.com>
2022-05-21 11:18:56 -0300

    Início do projeto

---
+++ src/test/java/br/com/waste/escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.waste.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
