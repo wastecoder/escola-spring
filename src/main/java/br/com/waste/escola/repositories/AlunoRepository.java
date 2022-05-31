8a5cf1c9e741a188f0e8b2b336fbbf569fcdb5c6
Paulino <paulinomendes03@gmail.com>
2022-05-31 02:31:24 -0300

    Adiciona mais duas opções de filtro no relatório com JPQL

--- src/main/java/br/com/waste/escola/repositories/AlunoRepository.java
+++ src/main/java/br/com/waste/escola/repositories/AlunoRepository.java
@@ -1,6 +1,7 @@
 package br.com.waste.escola.repositories;
 
 import br.com.waste.escola.models.Aluno;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,16 @@ import java.util.List;
 
 @Repository
 public interface AlunoRepository extends CrudRepository<Aluno, Long> {
+        //Derived query
     List<Aluno> findByNomeStartingWith(String nome);
+
     List<Aluno> findByNomeStartingWithAndIdadeLessThanEqual(String nome, Integer idade);
+
+
+        //JPQL
+    @Query("SELECT a FROM Aluno a WHERE a.nome like :nome% AND a.idade >= :idade")
+    List<Aluno> findNameEIdadeMaiorIgual(String nome, Integer idade);
+
+    @Query("SELECT a FROM Aluno a INNER JOIN a.disciplinas d WHERE a.nome like :nomeAluno% AND a.idade >= :idadeAluno AND d.nome like :nomeDisciplina%")
+    List<Aluno> findNameEIdadeMaiorIgualMatriculado(String nomeAluno, Integer idadeAluno, String nomeDisciplina);
 }
--- src/main/java/br/com/waste/escola/services/RelatorioAlunoService.java
+++ src/main/java/br/com/waste/escola/services/RelatorioAlunoService.java
@@ -21,13 +21,15 @@ public class RelatorioAlunoService {
         boolean continuar = true;
 
         do {
-            System.out.println("\n+------------------+");
-            System.out.println("|   BUSCAR ALUNO   |");
-            System.out.println("+------------------+");
-            System.out.println("| 0 - Voltar       |");
-            System.out.println("| 1 - Nome         |");
-            System.out.println("| 2 - 1 + idade <= |");
-            System.out.println("+------------------+");
+            System.out.println("\n+--------------------+");
+            System.out.println("|    BUSCAR ALUNO    |");
+            System.out.println("+--------------------+");
+            System.out.println("| 0 - Voltar         |");
+            System.out.println("| 1 - Início do nome |");
+            System.out.println("| 2 - 1 + idade <=   |");
+            System.out.println("| 3 - 1 + idade >=   |");
+            System.out.println("| 4 - 3 + matriculado|");
+            System.out.println("+--------------------+");
             System.out.print("Escolha: ");
             byte escolha = input.nextByte();
             input.nextLine();   // Evita o bug de String com espaços
@@ -37,7 +39,13 @@ public class RelatorioAlunoService {
                     this.byName(input);
                     break;
                 case 2:
-                    this.byNameAndAge(input);
+                    this.byNameAndAgeLessEqual(input);
+                    break;
+                case 3:
+                    this.byNameAndAgeGreatterEqual(input);
+                    break;
+                case 4:
+                    this.byNameAndAgeGreatterEqualAndEnrolled(input);
                     break;
                 default:
                     continuar = false;
@@ -56,7 +64,7 @@ public class RelatorioAlunoService {
         alunosNome.forEach(System.out::println);
     }
 
-    private void byNameAndAge(Scanner input) {
+    private void byNameAndAgeLessEqual(Scanner input) {
         System.out.println("\n>>> BUSCANDO: início do nome e idade <= x <<<");
 
         System.out.print("> nome: ");
@@ -66,6 +74,36 @@ public class RelatorioAlunoService {
         Integer idade = input.nextInt();
 
         List<Aluno> alunosNome = alunoRepository.findByNomeStartingWithAndIdadeLessThanEqual(nome, idade);
+        alunosNome.forEach(System.out::println);
+    }
+
+    private void byNameAndAgeGreatterEqual(Scanner input) {
+        System.out.println("\n>>> BUSCANDO: início do nome e idade >= x <<<");
+
+        System.out.print("> nome: ");
+        String nome = input.nextLine();
+
+        System.out.print("> idade: ");
+        Integer idade = input.nextInt();
+
+        List<Aluno> alunosNome = alunoRepository.findNameEIdadeMaiorIgual(nome, idade);
+        alunosNome.forEach(System.out::println);
+    }
+
+    private void byNameAndAgeGreatterEqualAndEnrolled(Scanner input) {
+        System.out.println("\n>>> BUSCANDO: nome, idade >= x e disciplina <<<");
+
+        System.out.print("> nome da disciplina: ");
+        String disciplina = input.nextLine();
+
+        System.out.print("> nome do aluno: ");
+        String nome = input.nextLine();
+
+        System.out.print("> idade do aluno: ");
+        Integer idade = input.nextInt();
+
+
+        List<Aluno> alunosNome = alunoRepository.findNameEIdadeMaiorIgualMatriculado(nome, idade, disciplina);
         alunosNome.forEach(System.out::println);
     }
 }
